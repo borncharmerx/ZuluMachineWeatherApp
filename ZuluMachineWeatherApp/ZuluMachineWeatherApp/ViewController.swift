21f4580319101600f82de3af0dfae04f412bb171
Sipho Motha <sipho.motha@standardbank.co.za>
2020-02-18 23:33:48 +0200

    Initial Push

---
+++ ZuluMachineWeatherApp/ZuluMachineWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZuluMachineWeatherApp
+//
+//  Created by Sipho Motha on 2020/02/18.
+//  Copyright © 2020 ZuluMachine House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
